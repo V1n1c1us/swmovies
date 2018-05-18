a228a2c8f82eb5f24fccf485683daef89bf75b23
V1n1c1us <v.f.diehl@gmail.com>
2018-05-17 21:45:35 -0300

    Implementação do HeaderComponent e PrincipalComponent

--- appswmovies/src/app/principal/principal.component.ts
+++ appswmovies/src/app/principal/principal.component.ts
@@ -1,15 +1,36 @@
+declare var $: any;
+
 import { Component, OnInit } from '@angular/core';
+import { Movies } from '../model/movies.model';
+import {SwserviceService} from '../swservice.service';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./principal.component.css'],
+  providers: [SwserviceService]
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  public movies: Movies[];
+  public movie: Movies;
+
+
+  constructor(private swService: SwserviceService) { }
+
 
   ngOnInit() {
+    this.swService.getFilms().subscribe(data => {
+      this.movies = data.results;
+      this.movies.forEach(movie => console.log(movie));
+    });
   }
+
+  // exibe os detalhes do respectivo filme (modal)
+  detalhes(movie: Movies) {
+    this.movie = movie;
+    $("#detalhes").modal('show');
+  }
+
 
 }
